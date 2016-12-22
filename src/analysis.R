2606e677ab3949c5be22761d23a89158f4f50031
muratmaga <muratmaga@outlook.com>
2016-12-21 23:45:27 -0800

    Matching it to paper

--- src/analysis.R
+++ src/analysis.R
@@ -1,6 +1,8 @@
-MM=proc.time()
+#PCA analysis of results. 
 library( ANTsR )
 library( geomorph )
+library( abind )
+library( Morpho )
 # set the base directory manually. This should point out to the data folder of the cloned repository.
 # calculations require large amount of RAM (64+ GB) to be available.
 bd=path.expand( "/scratch/mouse_CT_atlas/data/")
@@ -29,7 +31,7 @@ lmlist = list( )
 for ( i in 1:length( sims ) ) {
   lotx = c( defs[i], mats[i], sims[i] )
   wpts <- antsApplyTransformsToPoints( dim=3, points=templatePoints,
-      transformlist=lotx )
+                                       transformlist=lotx )
   lmlist[[ i ]] = wpts
 }
 mydims = c( nrow( lmlist[[1]] ), ncol( lmlist[[1]]), length(lmlist) )
@@ -44,11 +46,15 @@ load(paste(bd,"/LMs.RData",sep="/")) #LMs manually annotated  from the individua
 GPA=gpagen(LM)
 GPA.pca=plotTangentSpace(GPA$coords)
 exclude.lm=which(! as.character(1:51) %in% rownames(LM[,,1]))   #Total of six LMs were missing from various mice. Exlcuding those from the pseudoLMs.
-diGPA=gpagen(pseudoLM[-exclude.lm,,])
-diGPA.pca=plotTangentSpace(diGPA$coords)
-plot(GPA$Csize,diGPA$Csize,main="Centroid Size")
-plot(GPA.pca$pc.scores[,1],diGPA.pca$pc.scores[,1])
-for (i in 1:10) print(cor.test(GPA.pca$pc.scores[,i],diGPA.pca$pc.scores[,i])$estimate) #correlations between PC scores.
+pseudoGPA=gpagen(pseudoLM[-exclude.lm,,])
+pseudoGPA.pca=plotTangentSpace(pseudoGPA$coords)
+joint=abind(LM,pseudoLM[-exclude.lm,,])
+joint.GPA=gpagen(joint)
+
+par(mfrow=c(1,2))
+plot(GPA$Csize, pseudoGPA$Csize,xlab="Manual LMs Centroid Size",ylab="Pseudo LM Centroid Size",pch=20)
+plot(GPA.pca$pc.scores[,1],pseudoGPA.pca$pc.scores[,1],xlab="Manual LMs PC1",ylab="Pseudo LM PC1",pch=20)
+for (i in 1:5) print(cor.test(GPA.pca$pc.scores[,i],pseudoGPA.pca$pc.scores[,i]))
 
 #MEASURES OF SIZE
 Skulls=dir(path=paste(bd,'/targets/masked/registered/Skull/',sep=''),patt="gz",full.names = T)
@@ -61,6 +67,8 @@ voxel_count=function(X){
 skull_sizes=NULL
 for (i in Skulls) skull_sizes=c(skull_sizes, voxel_count(i))
 
+
+#Calculate deformation fields
 odr = paste(bd,"/targets/masked/registered/Skull/step1/step2/",sep='')
 jmean = rep( NA, length( defs ) )
 jlist = list()
@@ -74,7 +82,7 @@ for ( i in 1:length( defs ) ) {
   mymatparms = getAntsrTransformParameters( mymat )
   mymatparms[ c(1,5,9) ] = mymatparms[ c(1,5,9) ] * myscale
   newtx = createAntsrTransform( type = "AffineTransform", precision = "float",
-    dimension = 3, parameters=mymatparms, fixed.parameters=fixedparms )
+                                dimension = 3, parameters=mymatparms, fixed.parameters=fixedparms )
   newmatfn = paste( odr, samples[i], "_scaledaff.mat",sep='')
   writeAntsrTransform( newtx, newmatfn )
   mytx = c( defs[i], newmatfn )
@@ -93,25 +101,23 @@ for ( i in 1:length( defs ) ) wlist[[ i ]] = antsImageRead( defs[i] )
 #Compute the PCA basis on SyN portion only:
 
 pca_type='randPCA'  #randomized PCA requires rsvd package to be installed
-myk = 5
+
 if ( !exists("diGPA_SyN_PCA") )
   ####
-diGPA_SyN_PCA = multichannelPCA( wlist, msk, k=myk, pcaOption=pca_type, verbose=FALSE )
-plot(diGPA_SyN_PCA$pca$u[,1:2],pch=20,cex=2,main="SyN Only PCA")
+  diGPA_SyN_PCA = multichannelPCA( wlist, msk, pcaOption=pca_type, verbose=FALSE )
+
+plot(diGPA_SyN_PCA$pca$u[,1:2],pch=20,cex=2,main="SyN Only PCA",xlab="PC1", ylab="PC2")
 text(diGPA_SyN_PCA$pca$u[,1:2],labels=samples,cex=.6,pos=2)
+
 
 #Compute the PCA basis on total warp :
 if ( !exists("diGPA_total_PCA") )
   ####
-diGPA_total_PCA= multichannelPCA( ylist, msk, k=myk, pcaOption=pca_type, verbose=FALSE )
+  #diGPA_total_PCA= multichannelPCA( ylist, msk, k=myk, pcaOption=pca_type, verbose=FALSE )
+  diGPA_total_PCA= multichannelPCA( ylist, msk, pcaOption=pca_type, verbose=FALSE )
 
-summary( lm( log(skull_sizes) ~ diGPA_total_PCA$pca$u[,1:myk] ) )
-
-plot(diGPA_total_PCA$pca$u[,1:2],pch=20,cex=2,main="Total Warps PCA")
+plot(diGPA_total_PCA$pca$u[,1:2],pch=20,cex=2,main="Total Warps PCA",xlab="PC1", ylab="PC2")
 text(diGPA_total_PCA$pca$u[,1:2],labels=samples,cex=.6,pos=2)
-
-print(proc.time()-MM)
-
 
 #Now visualize the first two PC (positive component) and save the as volumes
 #these steps require rgl, misc3d and pixmap libraries installed in R
@@ -137,48 +143,8 @@ for ( ww in 1:mxk )
   par(mfrow=c(3,1))
   for (k in 1:3) plot( template, mgntd, window.overlay=c(0.1,1),axis=k )
   title(paste("Magnitude for +PC",ww))
-
+  
 }
-
-#allometry code from geomorph
-allometry <- procD.lm(GPA$coords~GPA$Csize,iter = 1)
-GPA.resid <- arrayspecs(allometry$residuals,
-                          p=dim(GPA$coords)[1], k=dim(GPA$coords)[2]) # size-adjusted residuals
-adj.shape <- GPA.resid + array(GPA$consensus, dim(GPA.resid)) # allometry-free shapes
-GPA.resi.pca=plotTangentSpace(adj.shape) # PCA of allometry-free shape
-
-par(mfrow=c(2,2))
-plot(GPA.pca$pc.scores[,1],diGPA_SyN_PCA$pca$u[,1],xlab="GPA PC1",ylab="diGPA SyN PC1",pch=20,cex=2,cex.axis=2)
-plot(GPA.resi.pca$pc.scores[,1],diGPA_SyN_PCA$pca$u[,1],xlab="GPA resi PC1",ylab="diGPA SyN PC1",pch=20,cex=2,cex.axis=2)
-plot(GPA.pca$pc.scores[,1],diGPA_total_PCA$pca$u[,1],xlab="GPA PC1",ylab="diGPA totalWarp PC1",pch=20,cex=2,cex.axis=2)
-plot(GPA.resi.pca$pc.scores[,1],diGPA_total_PCA$pca$u[,1],xlab="GPA resi PC1",ylab="diGPA totalWarp PC1",pch=20,cex=2,cex.axis=2)
-
-#correlations between corresponding PC scores.
-for (i in 1:10) {
-  cor.test(GPA.pca$pc.scores[,i],diGPA_SyN_PCA$pca$u[,i])
-  cor.test(GPA.resi.pca$pc.scores[,i],diGPA_SyN_PCA$pca$u[,i])
-  cor.test(GPA.pca$pc.scores[,i],diGPA_total_PCA$pca$u[,i])
-  cor.test(GPA.resi.pca$pc.scores[,i],diGPA_total_PCA$pca$u[,i])
-}
-
-par(mfrow=c(2,2))
-plot(round((diGPA_SyN_PCA$pca$d^2)/sum(diGPA_SyN_PCA$pca$d^2),3),pch=20,cex=2,cex.axis=2,main="diGPA SyN PCA",
-      ylab="Variance Explained",xlab="Principal Components")
-plot(round((diGPA_total_PCA$pca$d^2)/sum(diGPA_total_PCA$pca$d^2),3),pch=20,cex=2,cex.axis=2,main="diGPA Total Warp PCA",
-     ylab="Variance Explained",xlab="Principal Components")
-plot(round((GPA.pca$sdev^2)/sum(GPA.pca$sdev^2),3),pch=20,cex=2,cex.axis=2,main="GPA PCA",
-     ylab="Variance Explained",xlab="Principal Components")
-plot(round((GPA.resi.pca$sdev^2)/sum(GPA.resi.pca$sdev^2),3),pch=20,cex=2,cex.axis=2,main="Allometry free GPA PCA",
-     ylab="Variance Explained",xlab="Principal Components")
-
-plot(log(skull_sizes)~log(GPA$Csize),pch=20,cex=2,cex.axis=1.5,ylab="Skull Voxel Count",xlab="LM Centroid Size")
-
-#how does skull size impact the PC1 scores
-regress_diGPA_total=lm(diGPA_total_PCA$pca$u[,1]~log(skull_sizes))
-regress_diGPA_SyN=lm(diGPA_SyN_PCA$pca$u[,1]~log(skull_sizes))
-regress_GPA_pca=lm(GPA.pca$pc.scores[,1]~log(GPA$Csize))
-regress_GPA_resi.pca=lm(GPA.resi.pca$pc.scores[,1]~log(GPA$Csize))
-
 
 #R's default save function will not save the values stored in pointers 
 #please uncomments the line below, if you want to save your analysis. 
@@ -189,3 +155,53 @@ regress_GPA_resi.pca=lm(GPA.resi.pca$pc.scores[,1]~log(GPA$Csize))
 #and rerun Lines 6-22 to correct the filenames. 
 
 
+#plots and tables in the paper
+#Figure 3.
+plot(joint.GPA$consensus[,1:2],pch=32,col="black",cex=2)
+for (i in 1:51) points(joint.GPA$coords[,1:2,i],pch=20,col="red")
+for (i in 52:102) points(joint.GPA$coords[,1:2,i],pch=20,col="cyan")
+#points(joint.GPA$consensus[,1:2],pch=3,col="black",cex=2,lwd=4)
+pseudo_Mean=apply(joint.GPA$coords[,,52:102],c(1,2),mean)
+GPA_Mean=apply(joint.GPA$coords[,,1:51],c(1,2),mean)
+points(GPA_Mean[,1:2],col="black",pch=3,cex=2,lwd=2)
+points(pseudo_Mean[,1:2],col="black",pch=3,cex=2,lwd=2)
+
+groups=as.factor(c(rep("GPA",51),rep("Pseudo",51)))
+proc.anova=procD.lm(joint.GPA$coords~groups,iter = 1000,RRPP = T)
+
+
+
+#Figure 4.
+par(mfrow=c(1,2))
+plot(GPA$Csize,pseudoGPA$Csize,pch=20,cex=2,cex.axis=1.5,ylab="pseudoLM CS",xlab="LM CS")
+plot(log(skull_sizes)~log(GPA$Csize),pch=20,cex=2,cex.axis=1.5,ylab="log Skull Voxel Count",xlab="log LM Centroid Size")
+
+
+#Figure 5.
+par(mfrow=c(2,2))
+plot(round((GPA.pca$sdev^2)/sum(GPA.pca$sdev^2),3),pch=20,cex=2,cex.axis=2,main="GPA PCA",
+     ylab="Variance Explained",xlab="Principal Components")
+plot(round((GPA.pca$sdev^2)/sum(GPA.pca$sdev^2),3),pch=20,cex=2,cex.axis=2,main="Pseudo LM PCA",
+     ylab="Variance Explained",xlab="Principal Components")
+plot(round((diGPA_SyN_PCA$pca$d^2)/sum(diGPA_SyN_PCA$pca$d^2),3),pch=20,cex=2,cex.axis=2,main="diGPA SyN PCA",
+     ylab="Variance Explained",xlab="Principal Components")
+plot(round((diGPA_total_PCA$pca$d^2)/sum(diGPA_total_PCA$pca$d^2),3),pch=20,cex=2,cex.axis=2,main="diGPA Total Warp PCA",
+     ylab="Variance Explained",xlab="Principal Components")
+
+
+#Figure 6.
+par(mfrow=c(2,2))
+plot(GPA.pca$pc.scores[,1],pseudoGPA.pca$pc.scores[,1],pch=20, xlab="GPA PC1",ylab="Pseudo LM PC1")
+plot(GPA.pca$pc.scores[,1],diGPA_SyN_PCA$pca$u[,1],pch=20, xlab="GPA PC1",ylab="SyN PC1")
+plot(GPA.pca$pc.scores[,1],diGPA_total_PCA$pca$u[,1],pch=20, xlab="GPA PC1",ylab="Total Warp PC1")
+plot(diGPA_SyN_PCA$pca$u[,1],diGPA_total_PCA$pca$u[,1],pch=20, xlab="SyN PC1",ylab="Total Warp PC1")
+
+
+#correlations between corresponding PC scores.
+for (i in 1:1) {
+  print(cor.test(GPA.pca$pc.scores[,i],pseudoGPA.pca$pc.scores[,i]))
+  print(cor.test(GPA.pca$pc.scores[,i],diGPA_SyN_PCA$pca$u[,i]))
+  print(cor.test(GPA.pca$pc.scores[,i],diGPA_total_PCA$pca$u[,i]))
+  print(cor.test(diGPA_SyN_PCA$pca$u[,i],diGPA_total_PCA$pca$u[,i]))
+  
+}
